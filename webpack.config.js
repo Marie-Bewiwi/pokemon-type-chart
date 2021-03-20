192068f7c40b4b4ef1a5deb75135c2d4be40b892
Virac David AUK <auk.david@gmail.com>
2021-03-20 10:48:13 +0100

    Prepare prod!

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,14 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const CopyWebpackPlugin = require('copy-webpack-plugin')
 const path = require('path')
 
 const template = path.resolve(__dirname, 'src', 'index.html')
 
 module.exports = {
+  output: {
+    path: path.resolve(__dirname, 'docs'),
+    clean: true
+  },
   devtool: "source-map",
   module: {
     rules: [
@@ -24,7 +29,12 @@ module.exports = {
     }
   },
   plugins: [
-    new HtmlWebpackPlugin({ template })
+    new HtmlWebpackPlugin({ template }),
+    new CopyWebpackPlugin({
+      patterns: [
+        { from: path.resolve(__dirname, 'data'), to: path.resolve(__dirname, 'docs') }
+      ]
+    })
   ],
   devServer: {
     hot: true
